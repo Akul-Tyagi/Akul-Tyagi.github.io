10867f0904b509718cb6684c8507caba4c6ca278
Akul Tyagi <akultyagi7@gmail.com>
2025-10-05 20:14:43 +0530

    Some production deployment settings modified

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // skip strict mode
+  output: "export",
+  images: { unoptimized: true },
   reactStrictMode: false,
   env: {
     googleAnalyticsId: process.env.NODE_ENV === "production" ? process.env.GA_MEASUREMENT_ID : "",
